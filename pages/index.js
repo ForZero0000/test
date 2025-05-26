0bd85c161c485d66d14485ee7669414fd204c446
ForZero0000 <96049760+ForZero0000@users.noreply.github.com>
2025-05-26 02:15:17 +0200

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+export default function Home() {
+  return (
+    <div style={{
+      textAlign: 'center',
+      marginTop: '50px',
+      color: '#00BFFF',
+      backgroundImage: 'url(https://irys.xyz/og.png)',
+      backgroundSize: 'cover',
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'column',
+    }}>
+      <h1>Irys Love Checker</h1>
+      <p>Проверка на Sybil от Irys</p>
+    </div>
+  )
+}
